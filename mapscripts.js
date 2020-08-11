6c02a97328a1d52e377767831ea7345baef6f7f4
lukast <tinnes-lukas@gmx.de>
2020-08-11 22:11:43 +0200

    test map added

--- mapscripts.js
+++ mapscripts.js
@@ -1,3 +1,10 @@
-function test() {
-  console.log("Function was called");
+function homemap() {
+  let map = null;
+  console.log("Setting up map");
+  var map = L.map('mapid', {
+    crs: L.CRS.Simple
+});
+  var bounds = [[0,0], [1000,1000]];
+  var image = L.imageOverlay('map.png', bounds).addTo(map);
+  map.fitBounds(bounds);
 }
