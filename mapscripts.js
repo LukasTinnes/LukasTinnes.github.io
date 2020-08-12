76534707d04e87562b877dd9b87a43c590d597be
lukast <tinnes-lukas@gmx.de>
2020-08-12 16:31:11 +0200

    Signed-off-by: lukast <tinnes-lukas@gmx.de>

--- mapscripts.js
+++ mapscripts.js
@@ -94,9 +94,11 @@ var Bobby_die_Ratte = L.icon({
     popupAnchor:  [20, 20] // point from which the popup should open relative to the iconAnchor
 });
 
+}
 function saveToFile(content, filename) {
+      import FileSaver from "FileSaver.js"
       var file = filename + '.geojson';
-      saveAs(new File([JSON.stringify(content)], file, {
+      FileSaver.saveAs(new File([JSON.stringify(content)], file, {
         type: "text/plain;charset=utf-8"
       }), file);
     }
